49153fb994bcc0356d20b36c7358e98ff350c3fb
divyaramya <sridivyaganesan97@gmail.com>
2017-06-20 17:32:05 +0530

    Create paly.java

---
+++ paly.java
@@ -0,0 +1,14 @@
+public class palyndrom{
+public static void main(String[] args){
+String s="aba";
+String rev;
+
+rev=StringBuffer(s).reverse().to String();
+if(s==rev){
+System.out.println("It is palyndrom");
+}
+else{
+System.out.println("Not palyndrom");
+}
+}
+}
